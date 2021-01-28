c410bfaa142d28e48d6383a85d80799ccddf66d1
Xuan Zhong <xuan.zhong06@gmail.com>
2021-01-27 22:33:15 -0800

    loading wav files

--- practice/base_audio_io.py
+++ practice/base_audio_io.py
@@ -1,4 +1,10 @@
 # basic audio io
-
 import wave
+from scipy.io import wavfile
+import matplotlib.pyplot as plt
 
+FILE = '/Users/Xuan/PycharmProjects/thinkDSP/ThinkDSP/practice/speech_male.wav'
+fs, y = wavfile.read(FILE)
+plt.plot(y)
+plt.show()
+print('done')
